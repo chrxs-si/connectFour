1994edd97ef531d6ad2e03b7b44e60821493cd95
Luroka <christophleptin@gmail.com>
2025-07-29 23:09:39 +0200

    save

--- monteCarloTreeSearch.py
+++ monteCarloTreeSearch.py
@@ -4,32 +4,37 @@ from game import connectFour
 
 ITERATIONS = 400
 
-def monteCarloStep(oldcf, player, depth=0):
-  cf = deepcopy(oldcf)
-  cfPaths = []
-  for i in range(len(cf.field)): 
-    cfPaths.append([None, None])  #contains cf and path list with for the next depth
-  print(f'cfPath: {cfPaths}')
+class MCTSNode:
+  def __init__(self, cf, parent=None, move=None):
+    self.cf = cf                # Spielzustand
+    self.parent = parent              # Referenz zum Elternknoten
+    self.children = []                # Liste von Kindknoten
+    self.move = move                  # Der Zug, der zu diesem Zustand geführt hat
+    self.visits = 0                   # Wie oft wurde dieser Knoten besucht?
+    self.wins = 0                     # Wie viele Siege resultierten aus diesem Knoten?
+    self.untriedMoves = []  # Noch nicht ausprobierte Züge
+    for i in range(len(cf.field)):
+      if cf.field[i][0] == 0:
+        self.untriedMoves.append(i)
+
+
+def monteCarloStep(parentNode, move=None, player=None, depth=0):
+  node = MCTSNode(deepcopy(parentNode.cf), parent=parentNode, move=move)
 
   # Choose a random row that is not already full
-  available_rows = [i for i in range(len(cf.field)) if cf.field[i][0] == 0]
-  row = available_rows[randint(0, len(available_rows) - 1)]
-  print(row)
+  row = parentNode.untriedMoves[randint(0, len(parentNode.untriedMoves) - 1)]
+  parentNode.untriedMoves.remove(row)
 
-  win = cf.chooseRow(row)
-  print(f'win: {win}, depth: {depth}')
-  cfPaths[row][0] = cf
-  if win == 0: # Continue playing randomly until the game ends
-    cfPaths[row][1] = monteCarloStep(cf, player, depth+1) # Spiel noch nicht zu Ende
+  win = node.cf.chooseRow(row)
+  if win == 0:
+    node.children.append(monteCarloStep(node, row, player, depth+1)) # Spiel noch nicht zu Ende
+    node.wins += node.children[-1].wins
   elif win == -1:
-    cfPaths[row][1] = 0 # unentschieden
+    node.wins = 0 # unentschieden
   else:
-    cfPaths[row][1] = 1 if win == player else -1 # Spieler gewinnt
+    node.wins = 1 if win == player else -1 # Spieler gewinnt
 
-  print(f'depth: {depth}')
-
-  print(f'cfPath: {cfPaths}\n\n')
-  return cfPaths
+  return node
 
 
 def dicoverPaths(path):
@@ -37,19 +42,16 @@ def dicoverPaths(path):
     row = available_rows[randint(0, len(available_rows) - 1)]
 
 
-
 def findBestMove(cf, player):
 
-  parentsPaths = monteCarloStep(cf, player)
-  print(parentsPaths)
-  
-  while parentsPaths[1] == None or None in parentsPaths[1]:
-    available_rows = [i for i in range(len(cf.field)) if parentsPaths[1][i][0] is None]
-    row = available_rows[randint(0, len(available_rows) - 1)]
-    parentsPaths[1] = monteCarloStep(parentsPaths[0], player)
-  print(parentsPaths)
+  node = MCTSNode(deepcopy(cf))
+  while len(node.untriedMoves) > 0:
+    node.children.append(monteCarloStep(node, move=None, player=player, depth=0))
+    node.wins += node.children[-1].wins
+    node.visits += 1
+    print(f'node.wins: {node.wins}, node.visits: {node.visits}, node.untriedMoves: {node.untriedMoves}')
 
-    
+      
 
 
     
